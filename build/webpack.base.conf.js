aa7404acabeb30b5faab0b79d8925e8b478b2469
yjtmD <1017062100@qq.com>
2018-06-15 10:38:27 +0800

    add amap

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -67,6 +67,10 @@ module.exports = {
       }
     ]
   },
+  externals: {
+    'AMap': 'window.AMap',
+    'AMapUI': 'AMapUI'
+  },
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
--- src/moudle/home/index.vue
+++ src/moudle/home/index.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <headers></headers>
     <div>content</div>
+    <div id="map-container"></div>
     <footers :title="msg"></footers>
   </div>
 </template>
@@ -9,13 +10,17 @@
 <script>
   import {apiTest} from './api'
   import io from 'socket.io-client'
+  import AMap from 'AMap'
+  // AMapUI以后扩展可能用到
+  import AMapuUI from 'AMapUI'
 
   export default {
     name: 'HelloWorld',
     data () {
       return {
         msg: 'Welcome to Your Vue.js App',
-        testSocket: io.connect('ws://localhost:8088/test')
+        testSocket: io.connect('ws://localhost:8088/test'),
+        map: null
       }
     },
     mounted(){
@@ -24,6 +29,8 @@
       this.testSocket.on('get_test_msg', function (data) {
         _this.msg = data.msg
       })
+      // 初始化地图
+      this.mapInit()
     },
     destroyed(){
       this.testSocket.disconnect()
@@ -39,6 +46,12 @@
           .catch(err =>{
             console.log(err)
           })
+      },
+      mapInit () {
+        this.map = new AMap.Map('map-container', {
+          center: [120.387057,36.06142],
+          zoom: 9
+        })
       }
     }
   }
@@ -46,5 +59,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  #map-container {
+    width: 100%;
+    height: 500px;
+  }
 </style>
